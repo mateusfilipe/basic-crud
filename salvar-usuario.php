d2abb8bbb9fd89614c770b11c04ee606c0bd0cd2
Mateus Filipe <mateusfilipe557@gmail.com>
2023-01-05 20:50:54 -0300

    Adicionado páginas de usuário

---
+++ salvar-usuario.php
@@ -0,0 +1,14 @@
+<?php
+    switch($_REQUEST["acao"]){
+        case 'cadastrar':
+            //cadastrar
+            break;
+        case 'editar':
+            //editar
+            break;
+        case 'excluir':
+            //excluir
+            break;
+    }
+
+?>
