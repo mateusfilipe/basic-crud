f2e1cd4b4941a5a32e7f3b200fe3e85f604972d9
Mateus Filipe <mateusfilipe557@gmail.com>
2023-01-05 21:29:42 -0300

    criado arquivo para listar usuarios

--- listar-usuario.php
+++ listar-usuario.php
@@ -1,1 +1,36 @@
 <h1> Listar Usuários </h1>
+
+<?php
+    $sql = "SELECT * from usuarios";
+
+    $result = $conn->query($sql);
+
+    $qtd_usuarios = $result->num_rows;
+
+    if($qtd_usuarios = 0)
+    {
+        print "<p class='alert alert-danger'>Não encontrou nenhum resultado</p>";
+    }
+    print "<table class='table table-hover table-striped table-bordered'>";
+        print "<tr>";
+                print "<th>#</th>";
+                print "<th>Nome</th>";
+                print "<th>E-mail</th>";
+                print "<th>Data de Nascimento</th>";
+                print "<th>Ações</th>";
+            print "</tr>";
+        while($row = $result->fetch_object())
+        {
+            print "<tr>";
+                print "<td>" . $row->id . "</td>";
+                print "<td>" . $row->nome . "</td>";
+                print "<td>" . $row->email . "</td>";
+                print "<td>" . $row->data_nascimento . "</td>";
+                print "<td>
+                        <button class='btn btn-success'>Editar</button>
+                        <button class='btn btn-danger'>Excluir</button>
+                       </td>";
+            print "</tr>";
+        }
+    print "</table>";
+?>
