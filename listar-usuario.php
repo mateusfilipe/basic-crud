9bdac3ec365580ac2c34815b82945cb2013d9d38
Mateus Filipe <mateusfilipe557@gmail.com>
2023-01-05 22:14:55 -0300

    Add editar e excluir usuario

--- listar-usuario.php
+++ listar-usuario.php
@@ -27,8 +27,16 @@
                 print "<td>" . $row->email . "</td>";
                 print "<td>" . $row->data_nascimento . "</td>";
                 print "<td>
-                        <button class='btn btn-success'>Editar</button>
-                        <button class='btn btn-danger'>Excluir</button>
+                        <button onclick=\"location.href='?page=editar&id=".$row->id."'\" class='btn btn-success'>Editar</button>
+                        <button onclick=\"
+                                if(confirm('Tem certeza que deseja excluir?'))
+                                {
+                                    location.href='?page=salvar&acao=excluir&id=".$row->id."'
+                                }else
+                                {
+                                    false
+                                }\"
+                                class='btn btn-danger'>Excluir</button>
                        </td>";
             print "</tr>";
         }
--- salvar-usuario.php
+++ salvar-usuario.php
@@ -23,10 +23,39 @@
 
             break;
         case 'editar':
-            //editar
+            $nome      = $_POST['nome'];
+            $email     = $_POST['email'];
+            $senha     = md5($_POST['senha']);
+            $data_nasc = $_POST['data_nasc'];
+
+            $sql = "UPDATE usuarios SET
+                    nome  = '{$nome}',
+                    email = '{$email}',
+                    senha = '{$senha}',
+                    data_nascimento = '{$data_nasc}'
+                    WHERE id=".$_REQUEST['id'];
+
+            $result = $conn->query($sql);
+
+            if($result){
+                print "<script>alert('Usuário editado com sucesso!');</script>";
+                print "<script>locatio.href=?page=listar;</script>";
+            }else{
+                print "<script>alert('Não foi possível editar o usuário.);</script>";
+                print "<script>locatio.href=?page=listar;</script>";
+            }
             break;
         case 'excluir':
-            //excluir
+            $sql = "DELETE FROM usuarios WHERE id=".$_REQUEST['id'];
+            $result = $conn->query($sql);
+
+            if($result){
+                print "<script>alert('Usuário excluído com sucesso!');</script>";
+                print "<script>locatio.href=?page=listar;</script>";
+            }else{
+                print "<script>alert('Não foi possível excluir o usuário.);</script>";
+                print "<script>locatio.href=?page=listar;</script>";
+            }
             break;
     }
 
